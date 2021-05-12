8c407520b51cf486c866da5876371f0d5813e98f
Xiaochen Xu <xiaochex@gmail.com>
2021-05-11 21:03:24 -0400

    commit tree 1b60fd90c592c7d2f0a9a6532c5ce2827415e853

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,18 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(phrase)
+    if phrase == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    end
+    if phrase != phrase.upcase
+        return "HUH?! SPEAK UP, SONNY!"
+    else
+        return "NO, NOT SINCE 1938!"
+    end
+end
+
+
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
